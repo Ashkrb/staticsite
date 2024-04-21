035f8e5849e180b85d434b9f5f70e088bf24dd06
Ashkrb <krenczb@gmail.com>
2024-04-21 09:14:47 +0200

    finished markdown->htmlnoide conversion

--- src/splitblocks.py
+++ src/splitblocks.py
@@ -32,6 +32,35 @@ def markdown_to_blocks(markdown):
         blocks[i] = blocks[i].strip()
     return blocks
 
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    children = []
+    for block in blocks:
+        html_node = block_to_html_node(block)
+        children.append(html_node)
+    return ParentNode("div", children, None)
+
+def block_to_html_node(block):
+    block_type = block_to_block_type(block)
+    if block_type == block_type_paragraph:
+        return paragraph_to_html_node(block)
+    if block_type == block_type_heading:
+        return heading_to_html_node(block)
+    if block_type == block_type_code:
+        return code_to_html_node(block)
+    if block_type == block_type_olist:
+        return olist_to_html_node(block)
+    if block_type == block_type_ulist:
+        return ulist_to_html_node(block)
+    if block_type == block_type_quote:
+        return quote_to_html_node(block)
+    raise ValueError("Invalid block type")
+
+
+
+
+
 def block_to_block_type(text):
     if text is None:
         raise Exception("no block found")
